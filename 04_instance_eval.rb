26da3ded65500d351f7d74ef0990187ac9884cac
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2017-03-31 23:21:46 +0600

    Added some more

---
+++ 04_instance_eval.rb
@@ -0,0 +1,25 @@
+class Report
+  def initialize(&block)
+    puts "Header"
+    instance_eval &block if block_given?
+    puts "Footer"
+  end
+
+  def my_print(str)
+    puts str
+  end
+end
+
+Report.new() do
+  my_print "From block"
+end
+
+
+# Output
+=begin
+
+Header
+From block
+Footer
+
+=end
---
+++ 05_instance_eval.rb
@@ -0,0 +1,38 @@
+class Report
+  def initialize(data, &block)
+    @data = data
+    @columns = []
+    instance_eval &block if block_given?
+  end
+
+  def column(column_name)
+    @columns << column_name
+  end
+
+  def print
+    @data.each do |row|
+      @columns.each do |column|
+        puts row[column]
+      end
+    end
+  end
+end
+
+
+data = [
+  {name: 'Jitu', status: 'Married'},
+  {name: 'Razeen', status: 'Single'}
+]
+report = Report.new(data) do
+  column :name
+end
+
+report.print()
+
+# Output
+=begin
+
+Jitu
+Razeen
+
+=end
